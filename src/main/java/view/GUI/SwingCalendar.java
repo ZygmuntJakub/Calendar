d3f4d677b63768cc7521fc0d4ca06e09edfb5147
ZygmuntJakub <216938@edu.p.lodz.pl>
2019-06-04 12:04:35 +0200

    Dodanie kolorow do wydarzen

--- src/main/java/view/GUI/SwingCalendar.java
+++ src/main/java/view/GUI/SwingCalendar.java
@@ -1,51 +1,89 @@
 package view.GUI;
 
 import com.toedter.calendar.JCalendar;
+import model.Event;
 import model.Languages;
 
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.*;
+import java.awt.event.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.sql.Date;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 
 public class SwingCalendar extends JPanel {
 
+    JPanel jPanel;
     private static JCalendar calendar = new JCalendar();
     private Calendar c;
 
-    SwingCalendar(){
+    SwingCalendar() {
+        calendar.getDayChooser().setAlwaysFireDayProperty(false);
         c = Calendar.getInstance();
-        calendar.addPropertyChangeListener("calendar", new PropertyChangeListener() {
-            public void propertyChange(PropertyChangeEvent evt) {
-                c = (Calendar) evt.getNewValue();
-                EventEditorWindow eventEditorWindow = new EventEditorWindow(c);
-                SwingConsole.run(eventEditorWindow, 800, 500);
-
-            }
-        });
 
         changeVisible(false);
-        add(calendar);
+
+        jPanel = calendar.getDayChooser().getDayPanel();
+
+        Component compo[] = jPanel.getComponents();
+        for (Component comp : compo) {
+            if (!(comp instanceof JButton))
+                continue;
+            JButton btn = (JButton) comp;
+            changeButtonColor(btn);
+            calendar.addPropertyChangeListener("calendar", new PropertyChangeListener() {
+                public void propertyChange(PropertyChangeEvent evt) {
+                    c = (Calendar) evt.getNewValue();
+                    changeButtonColor(btn);
+                }
+            });
+            btn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    c = new GregorianCalendar(calendar.getYearChooser().getYear(), calendar.getMonthChooser().getMonth(), Integer.valueOf(btn.getText()));
+                    EventEditorWindow eventEditorWindow = new EventEditorWindow(c);
+                    eventEditorWindow.addWindowListener(new WindowAdapter() {
+                        @Override
+                        public void windowClosed(WindowEvent e) {
+                            changeButtonColor(btn);
+                        }
+                    });
+                    SwingConsole.run(eventEditorWindow, 800, 500);
+                }
+            });
+        }
+
+        add(calendar.getYearChooser());
+        add(calendar.getMonthChooser());
+        add(jPanel);
     }
 
-    public static void changeLanguage(Languages language){
-        if(language.equals(Languages.Polski)){
+    public static void changeLanguage(Languages language) {
+        if (language.equals(Languages.Polski)) {
             calendar.setLocale(new Locale("pl", "PL"));
-        } else if(language.equals(Languages.English)){
+        } else if (language.equals(Languages.English)) {
             calendar.setLocale(new Locale("en", "EN"));
         }
 
         calendar.updateUI();
     }
 
-    public static void changeVisible(boolean visible){
-        if(visible){
+    public static void changeVisible(boolean visible) {
+        if (visible) {
             calendar.setVisible(true);
-        } else{
+        } else {
             calendar.setVisible(false);
+        }
+    }
+    private void changeButtonColor(JButton btn){
+        if(EventEditorWindow.repoController.getDays(c).contains(btn.getText())){
+            btn.setBackground(Color.RED);
+            btn.setOpaque(true);
+        }else{
+            btn.setOpaque(false);
         }
     }
 }
