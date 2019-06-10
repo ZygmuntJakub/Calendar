f21204fda8c718a5ea5d501492d13cd33966af0e
ZygmuntJakub <216938@edu.p.lodz.pl>
2019-06-10 20:19:05 +0200

    wymiary okna na stałe

--- src/main/java/view/GUI/SwingConsole.java
+++ src/main/java/view/GUI/SwingConsole.java
@@ -16,6 +16,7 @@ public class SwingConsole {
     public static void run(final JFrame f, final int width, final int height){
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                f.setResizable(false);
                 f.setSize(width, height);
                 f.setVisible(true);
             }
